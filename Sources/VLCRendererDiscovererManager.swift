2900c4d020d0e3b17550b40fe2d10759ed939e84
Soomin Lee <bubu@mikan.io>
2018-03-12 17:45:48 +0100

    VLCRendererDiscovererManager: Implementation

---
+++ Sources/VLCRendererDiscovererManager.swift
@@ -0,0 +1,64 @@
+/*****************************************************************************
+ * VLCRendererDiscovererManager.swift
+ *
+ * Copyright © 2018 VLC authors and VideoLAN
+ * Copyright © 2018 Videolabs
+ *
+ * Authors: Soomin Lee <bubu@mikan.io>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+class VLCRendererDiscovererManager: NSObject, VLCRendererDiscovererDelegate  {
+
+    @objc static let sharedInstance = VLCRendererDiscovererManager();
+
+    private lazy var discoverers: Array<VLCRendererDiscoverer> = [VLCRendererDiscoverer]()
+    private lazy var renderers: Array<VLCRendererItem> = [VLCRendererItem]()
+
+    private override init() {
+        super.init()
+    }
+
+    @objc func start() -> Bool {
+        // Gather potential renderer discoverers
+        guard let tmpDiscoverers: Array<VLCRendererDiscovererDescription> = VLCRendererDiscoverer.list() else {
+            return false
+        }
+
+        for discoverer in tmpDiscoverers {
+            // Create new VLCRendererDiscoverers
+            if let rendererDiscoverer = VLCRendererDiscoverer(name: discoverer.name) {
+                rendererDiscoverer.start()
+                discoverers.append(rendererDiscoverer)
+            } else {
+                print("Unable to start renderer with name: \(discoverer.name)")
+            }
+        }
+        return true
+    }
+
+    @objc func stop() {
+        for discoverer in discoverers {
+            discoverer.stop()
+        }
+    }
+
+    //mark: RendererDiscovererDelegate
+    func rendererDiscovererItemAdded(_ rendererDiscoverer: VLCRendererDiscoverer, item: VLCRendererItem) {
+        print("RendererDiscovererManager: New item added")
+        renderers.append(item)
+    }
+
+    func rendererDiscovererItemDeleted(_ rendererDiscoverer: VLCRendererDiscoverer, item: VLCRendererItem) {
+        if let index = renderers.index(of: item) {
+            //rendererItems should be deallocated here, therefore calling item_release
+            print("RendererDiscovererManager: item removed")
+            renderers.remove(at: index)
+        } else {
+            //might already be removed
+            print("Issue while removing rendererItem: \(item)")
+        }
+    }
+
+}
