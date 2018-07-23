0d29b50a6ac18e0d8992501469640adebfd7e190
Soomin Lee <bubu@mikan.io>
2018-07-23 17:46:37 +0200

    AppCoordinator: Rename medialibrary service

--- Sources/Coordinators/AppCoordinator.swift
+++ Sources/Coordinators/AppCoordinator.swift
@@ -13,8 +13,7 @@ import Foundation
 
 @objc(VLCService)
 class Services: NSObject {
-    @objc let mediaDataSource = VLCMediaDataSource()
-    @objc let medialibrary = VLCMediaLibraryManager()
+    @objc let medialibraryManager = VLCMediaLibraryManager()
     @objc let rendererDiscovererManager = VLCRendererDiscovererManager(presentingViewController: nil)
 }
 
