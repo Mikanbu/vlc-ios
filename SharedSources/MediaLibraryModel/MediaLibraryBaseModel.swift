c3b150db61919ba686636c6c65db9881ddecc8e6
Soomin Lee <bubu@mikan.io>
2018-07-25 16:33:35 +0200

    MediaLibraryBaseModel: Update

--- SharedSources/MediaLibraryModel/MediaLibraryBaseModel.swift
+++ SharedSources/MediaLibraryModel/MediaLibraryBaseModel.swift
@@ -9,6 +9,30 @@
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
 
-protocol MediaLibraryBaseModel {
-
+extension Notification.Name {
+    static let VLCMoviesDidChangeNotification = Notification.Name("MoviesDidChangeNotfication")
+    static let VLCEpisodesDidChangeNotification = Notification.Name("EpisodesDidChangeNotfication")
+    static let VLCArtistsDidChangeNotification = Notification.Name("ArtistsDidChangeNotfication")
+    static let VLCAlbumsDidChangeNotification = Notification.Name("AlbumsDidChangeNotfication")
+    static let VLCTracksDidChangeNotification = Notification.Name("TracksDidChangeNotfication")
+    static let VLCGenresDidChangeNotification = Notification.Name("GenresDidChangeNotfication")
+    static let VLCAudioPlaylistsDidChangeNotification = Notification.Name("AudioPlaylistsDidChangeNotfication")
+    static let VLCVideoPlaylistsDidChangeNotification = Notification.Name("VideoPlaylistsDidChangeNotfication")
+    static let VLCVideosDidChangeNotification = Notification.Name("VideosDidChangeNotfication")
 }
+
+protocol MediaLibraryBaseModel {
+    associatedtype MLType where MLType: VLCMLObject
+
+    var files: [MLType] { get set }
+
+    var indicatorName: String { get }
+    var notificaitonName: Notification.Name { get }
+
+    // mutating will depend if we need to handle struc/enum
+    func append(_ item: MLType)
+    func isIncluded(_ item: MLType)
+}
+
+// protocol can be extended to have the "generic methods" that
+// childs will share. No need an in-between class
