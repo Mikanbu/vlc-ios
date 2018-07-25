16c8b3509482002f5e35ccfd15d813ec08ff0127
Soomin Lee <bubu@mikan.io>
2018-07-25 13:52:26 +0200

    Add VideoModel

---
+++ SharedSources/MediaLibraryModel/VideoModel.swift
@@ -0,0 +1,14 @@
+/*****************************************************************************
+ * VideoModel.swift
+ *
+ * Copyright © 2018 VLC authors and VideoLAN
+ * Copyright © 2018 Videolabs
+ *
+ * Authors: Soomin Lee <bubu@mikan.io>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+class VideoModel: MediaLibraryBaseModel {
+
+}
