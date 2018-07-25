4af5c2ab81753f19f387d1e81e14b9f1f03b7a0c
Soomin Lee <bubu@mikan.io>
2018-07-25 13:50:52 +0200

    Add MediaLibraryBaseModel

---
+++ SharedSources/MediaLibraryModel/MediaLibraryBaseModel.swift
@@ -0,0 +1,14 @@
+/*****************************************************************************
+ * MediaLibraryBaseModel.swift
+ *
+ * Copyright © 2018 VLC authors and VideoLAN
+ * Copyright © 2018 Videolabs
+ *
+ * Authors: Soomin Lee <bubu@mikan.io>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+protocol MediaLibraryBaseModel {
+
+}
