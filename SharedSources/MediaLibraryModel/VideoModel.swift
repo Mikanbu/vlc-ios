48cdcb25552242b32ae1ffdd858bd3b8755dc288
Soomin Lee <bubu@mikan.io>
2018-07-25 16:33:55 +0200

    VideoModel: Conform to MediaLibraryBaseModel

--- SharedSources/MediaLibraryModel/VideoModel.swift
+++ SharedSources/MediaLibraryModel/VideoModel.swift
@@ -9,6 +9,18 @@
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
 
-class VideoModel: MediaLibraryBaseModel {
+struct VideoModel: MediaLibraryBaseModel {
+    typealias MLType = VLCMLMedia
 
+    var files: [VLCMLMedia] = []
+
+    var indicatorName: String = NSLocalizedString("MOVIES", comment: "")
+
+    var notificaitonName: Notification.Name = .VLCVideosDidChangeNotification
+
+    func isIncluded(_ item: VLCMLMedia) {
+    }
+
+    func append(_ item: VLCMLMedia) {
+    }
 }
