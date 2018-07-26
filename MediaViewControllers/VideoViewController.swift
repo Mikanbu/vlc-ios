49a38aeea168bff17a400264f2c659200dc91ef9
Soomin Lee <bubu@mikan.io>
2018-07-26 15:36:57 +0200

    VideoViewController: fixup: don't pass category

--- MediaViewControllers/VideoViewController.swift
+++ MediaViewControllers/VideoViewController.swift
@@ -27,7 +27,7 @@ class VLCVideoViewController: VLCMediaViewController {
 
     override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
         return [
-            VLCMovieCategoryViewController(services, category: mediaCategory.movies),
+            VLCMovieCategoryViewController(services),
             VLCShowEpisodeCategoryViewController(services),
             VLCVideoPlaylistCategoryViewController(services)
         ]
