98a2e3e78125cfc17ba8760bcffaf6d1d576fa35
Soomin Lee <bubu@mikan.io>
2018-07-26 15:34:55 +0200

    VideoModel: Update

--- SharedSources/MediaLibraryModel/VideoModel.swift
+++ SharedSources/MediaLibraryModel/VideoModel.swift
@@ -9,18 +9,37 @@
  * Refer to the COPYING file of the official project for license.
  *****************************************************************************/
 
-struct VideoModel: MediaLibraryBaseModel {
+class VideoModel: MediaLibraryBaseModel {
     typealias MLType = VLCMLMedia
 
-    var files: [VLCMLMedia] = []
+    var files = [VLCMLMedia]()
 
     var indicatorName: String = NSLocalizedString("MOVIES", comment: "")
 
     var notificaitonName: Notification.Name = .VLCVideosDidChangeNotification
 
+    required init(medialibrary: VLCMediaLibraryManager) {
+        medialibrary.addObserver(self)
+    }
+
+    @objc func dataChanged() {
+        print("VideoModel: Notification: DataChanged")
+    }
+
     func isIncluded(_ item: VLCMLMedia) {
     }
 
     func append(_ item: VLCMLMedia) {
+        // need to check more for duplicate and stuff
+        files.append(item)
+    }
+}
+
+extension VideoModel: MediaLibraryObserver {
+    func medialibrary(_ medialibrary: VLCMediaLibraryManager, didAddVideo video: [VLCMLMedia]) {
+        print("VideoModel: didAddVideo: \(video.count)")
+        video.forEach({ append($0) })
+        // yikes
+        NotificationCenter.default.post(name: .VLCVideosDidChangeNotification, object: nil)
     }
 }
