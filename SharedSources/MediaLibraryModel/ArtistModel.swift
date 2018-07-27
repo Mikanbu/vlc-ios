ca55f442c964517379107f0965a4fa925f86c69e
Soomin Lee <bubu@mikan.io>
2018-07-27 16:07:22 +0200

    Add ArtistModel

---
+++ SharedSources/MediaLibraryModel/ArtistModel.swift
@@ -0,0 +1,42 @@
+/*****************************************************************************
+ * ArtistModel.swift
+ *
+ * Copyright © 2018 VLC authors and VideoLAN
+ * Copyright © 2018 Videolabs
+ *
+ * Authors: Soomin Lee <bubu@mikan.io>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+class ArtistModel: MediaLibraryBaseModel {
+    typealias MLType = VLCMLArtist
+
+    var files = [VLCMLArtist]()
+
+    var indicatorName: String = NSLocalizedString("ARTISTS", comment: "")
+
+    var notificaitonName: Notification.Name = .VLCArtistsDidChangeNotification
+
+    required init(medialibrary: VLCMediaLibraryManager) {
+        medialibrary.addObserver(self)
+        files = medialibrary.getArtists()
+    }
+
+    func isIncluded(_ item: VLCMLArtist) {
+    }
+
+    func append(_ item: VLCMLArtist) {
+        // need to check more for duplicate and stuff
+        files.append(item)
+    }
+}
+
+extension ArtistModel: MediaLibraryObserver {
+    func medialibrary(_ medialibrary: VLCMediaLibraryManager, didAddArtist artist: [VLCMLArtist]) {
+        print("ArtistModel: didAddArtists: \(artist.count)")
+        artist.forEach({ append($0) })
+        // yikes
+        NotificationCenter.default.post(name: notificaitonName, object: nil)
+    }
+}
