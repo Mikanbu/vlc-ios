a80dd1a12a74e5089ead55ebfc2e1e756ff14776
Soomin Lee <bubu@mikan.io>
2019-03-21 15:49:19 +0100

    SearchController: Initial

---
+++ Sources/Search/SearchController.swift
@@ -0,0 +1,14 @@
+/*****************************************************************************
+ * SearchController.swift
+ *
+ * Copyright © 2019 VLC authors and VideoLAN
+ *
+ * Authors: Soomin Lee <bubu # mikan.io>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+@objc(VLCSearchController)
+class SearchController: NSObject {
+
+}
